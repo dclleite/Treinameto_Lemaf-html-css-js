52762f24e14248f3e6e0ede2fa953f4190cd3f21
Eduardo Leite <eduappsplay@gmail.com>
2019-04-16 16:54:39 -0300

    adicionando evento click no botão de Adcionar Pacientes, e pegando os valores digitados pelo usuario

--- JavaScript_ProgramandoNaLinguagemDaWeb/introducao-javascript/js/principal.js
+++ JavaScript_ProgramandoNaLinguagemDaWeb/introducao-javascript/js/principal.js
@@ -11,8 +11,6 @@ for (var i = 0; i < pacientes.length; i++) {
     var alturaEhValido = true;
     var pesoEhValido = true;
     
-    
-    
     if (peso <= 0 || peso > 600 ) {
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido!";
@@ -23,8 +21,55 @@ for (var i = 0; i < pacientes.length; i++) {
     }
     if(alturaEhValido && pesoEhValido){
         tdImc.textContent = (peso / (altura * altura)).toFixed(2);
-    }else{
+    }else{ 
         paciente.classList.add("paciente-invalido")
     }
 
 }
+
+// Pegando evento de click do botão adicionar
+
+var btnAdicionar = document.querySelector("#adicionar-paciente");
+
+btnAdicionar.addEventListener("click", function(event){
+   event.preventDefault();
+    //pegando meu form do HTML
+    var form = document.querySelector("#form-adiciona");
+    
+    //pegando os valores digitados nas inputs
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    //criando uma tr para mais um paciente
+    var pacienteTr = document.createElement("tr");
+    
+    //criando as tds de mais um paciente
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    
+    //adicionando os valores digitados nas tds
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    //adicionando as tds como filhos da tr, seguindo o formato padrão do html
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    
+    //pegando tbody do html
+    var tabela = document.querySelector("#tabela-pacientes");
+    
+    //colocando a tr como filho dentro da tbody
+    tabela.appendChild(pacienteTr);
+    
+
+    
+    
+});
