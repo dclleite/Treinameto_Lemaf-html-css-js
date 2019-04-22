f44708fa532aa3c5c8b7b1b9a06491d9450d8da8
Eduardo Leite <eduappsplay@gmail.com>
2019-04-22 14:36:48 -0300

    Criando um DataHelper estatico, para converter texto para data, e data para texto

---
+++ javascript-avancado/aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,25 @@
+class DateHelper {
+
+    constructor(){
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
+        
+        /*return data.getDate() +
+            '/' + (data.getMonth() + 1) +
+            '/' + data.getFullYear();
+        */
+        //TemplateString
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
+    }
+    
+    static textoParaData(texto) {
+        //dentro do if está sendo usado uma expressão regular
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw('Deve estar no formato aaaa-mm-dd');
+        
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        
+    }
+}
