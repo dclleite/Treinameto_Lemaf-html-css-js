24b4eb9dd7d1cea1e75f037347443ae5f3d6fa43
Eduardo Leite <eduappsplay@gmail.com>
2019-04-22 17:17:23 -0300

    colocando Herança nas classes views

--- javascript-avancado/aluraframe/client/js/app/views/MensagemView.js
+++ javascript-avancado/aluraframe/client/js/app/views/MensagemView.js
@@ -1,17 +1,14 @@
-class MensagemView{
-    
-      constructor(elemento) {
-            this._elemento = elemento;
-      }
+class MensagemView extends View {
 
-      _template(model) {
 
-          // if ternario, se model.texto estiver vazio, retorna um paragrafo sem nada
+    constructor(elemento) {
+        super(elemento);
+    }
+
+
+    _template(model) {
+
+        // if ternario, se model.texto estiver vazio, retorna um paragrafo sem nada
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
-      }
-    
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
     }
 }
