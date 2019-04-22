6d54edfefaa91d159854ef3524ab93b2cd829615
Eduardo Leite <eduappsplay@gmail.com>
2019-04-22 14:59:22 -0300

    Foi criado novo modelo, uma lista de negociacoes

--- javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,25 +1,39 @@
-class NegociacaoController{
-    
-    constructor(){
-        
-        this._inputQuantidade = document.querySelector('#quantidade'); 
-        this._inputData = document.querySelector('#data'); 
-        this._inputValor = document.querySelector('#valor'); 
+class NegociacaoController {
+
+    constructor() {
+
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputData = document.querySelector('#data');
+        this._inputValor = document.querySelector('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+
     }
-    
-    adiciona(event){
-        
+
+    adiciona(event) {
+
         event.preventDefault();
-           
-        let negociacao = new Negociacao(
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaForm()
+
+        console.log(this._listaNegociacoes.getNegociacoes());
+
+
+    }
+
+    _criaNegociacao(){
+        
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value  
-        );
-
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+            this._inputValor.value);
+    }
+    
+    _limpaForm() {
+        this._inputData.value = '';
+        this._inputQuantidade = 1;
+        this._inputValor = 0.0;
         
-        
+        this._inputData.focus();
     }
 }
---
+++ javascript-avancado/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,16 @@
+class ListaNegociacoes{
+    constructor(){
+        
+        this._negociacoes = []
+    }
+    
+    adiciona(negociacao){
+        
+        this._negociacoes.push(negociacao);
+    }
+    
+    getNegociacoes(){
+        
+        return this._negociacoes;
+    }
+}
